8af6c9cb8cd49d4bc8b9684fefa6157b32c52db1
0froq <sayhola@froq.me>
2026-03-25 14:04:12 +0800

    refactor(mini-statusline): remove diagnostics section

--- lua/plugins/mini-statusline.lua
+++ lua/plugins/mini-statusline.lua
@@ -15,7 +15,7 @@ return {
       content = {
         active = function()
           local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-          local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+          -- local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
           local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
           local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
           local location      = MiniStatusline.section_location({ trunc_width = 75 })
@@ -23,7 +23,6 @@ return {
 
           return MiniStatusline.combine_groups({
             { hl = mode_hl,                 strings = { mode } },
-            { hl = 'MiniStatuslineDevinfo', strings = { diagnostics } },
             '%<', -- Mark general truncate point
             { hl = 'MiniStatuslineFilename', strings = { filename } },
             '%=', -- End left alignment
