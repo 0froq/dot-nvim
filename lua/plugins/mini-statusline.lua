b64ed07e0204f70ef4c41364cc61acaa0a86f1ff
0froq <sayhola@froq.me>
2026-03-26 16:23:24 +0800

    feat(plugin): modify `mini-statusline` to adjust truncate length

--- lua/plugins/mini-statusline.lua
+++ lua/plugins/mini-statusline.lua
@@ -14,20 +14,19 @@ return {
       set_vim_settings = false,
       content = {
         active = function()
-          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 150 })
           -- local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-          local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
-          local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+          local filename      = MiniStatusline.section_filename({ trunc_width = 150 })
+          local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 150 })
           local location      = MiniStatusline.section_location({ trunc_width = 75 })
-          local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
 
           return MiniStatusline.combine_groups({
-            { hl = mode_hl,                 strings = { mode } },
+            { hl = mode_hl,                  strings = { mode } },
             '%<', -- Mark general truncate point
             { hl = 'MiniStatuslineFilename', strings = { filename } },
             '%=', -- End left alignment
             { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-            { hl = mode_hl,                  strings = { search, location } },
+            { hl = mode_hl,                  strings = { location } },
           })
         end,
         inactive = function()
