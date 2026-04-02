3b4f1391cc9afa1b497bb2bf721fc2517248ce23
0froq <sayhola@froq.me>
2026-04-02 15:44:15 +0800

    feat(mappings): improve scroll and add center keymaps
    - Change PageUp/PageDown to nvmap for visual mode support
    - Add G keymap with zz to center after jumping to bottom
    - Add zz keymap in insert mode batch for centering cursor

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,15 +10,17 @@ useMap.nvmap({ 'j', '<Down>' }, 'gj', 'Down')
 useMap.nvmap({ 'k', '<Up>' }, 'gk', 'Up')
 useMap.nvmap('<Left>', 'h', 'Left')
 useMap.nvmap('<Right>', 'l', 'Right')
-useMap.nmap('<PageUp>', '5kzz', 'Scroll Up')
-useMap.nmap('<PageDown>', '5jzz', 'Scroll Down')
+useMap.nvmap('<PageUp>', '5kzz', 'Scroll up')
+useMap.nvmap('<PageDown>', '5jzz', 'Scroll down')
+useMap.nvmap('G', 'Gzz', { desc = 'Scroll to bottom', noremap = true, silent = true })
 
 useMap.map({ 'n', 'v', 'x', 's', 'o' }, ';', ':', { noremap = true })
 
 useMap.batch({
   mode = { 'i', 't' },
   { 'aa', '<ESC>',           'Escape insert mode' },
-  { 'ww', '<ESC><cmd>w<cr>', 'Escape insert mode' }
+  { 'ww', '<ESC><cmd>w<cr>', 'Escape insert mode' },
+  { 'zz', '<ESC>zza',        'Center'}
 })
 
 useMap.nmap('<leader>nh', '<cmd>noh<CR>', 'General clear highlights')
